be40772e781316b659ec38452bd0fe53313fa298
Lucian Mogosanu <lucian.mogosanu@gmail.com>
2014-12-11 20:03:46 +0200

    bot: remove useless whitespace

--- bot.py
+++ bot.py
@@ -41,14 +41,14 @@ class TestBot(irc.bot.SingleServerIRCBot):
     def do_command(self, e, cmd, private=True):
         nick = e.source.nick
         c = self.connection
-    
+
         cmd = cmd.split()
         if len(cmd) == 0:
             c.notice(nick, "Not understood")
             return
         if cmd[0] == "roll":
             rolls = str(nick) + " rolls " 
-            
+
             if len(cmd) >= 2:
                 for roll in cmd[1:]:
                     v = roll.split('d')
@@ -112,8 +112,8 @@ class TestBot(irc.bot.SingleServerIRCBot):
         c.notice(nick, "!roll [[xd]y]... rools x dice with y faces, default 1d6")
         c.notice(nick, "!grab [user [index]] saves a line to quotes")
         c.notice(nick, "!quote [user] produces a random quote")
-    
-    
+
+
 def sighandler(signum, frame):
     if bot != None:
         bot.stop()
@@ -138,7 +138,7 @@ def main():
     channel = "#" + sys.argv[2]
     nickname = sys.argv[3]
 
-    
+
     db = QuoteDB(sys.argv[2] + '.db')
     bot = TestBot(db, channel, nickname, server, port)
     signal.signal(signal.SIGINT, sighandler)
